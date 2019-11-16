2a22fe61a537fd8053418afcd7bf9525c344fe34
Tiago Dias <tiagodias@Tiagos-MacBook-Pro.local>
2019-11-16 21:13:44 +0000

    Initial commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,28 @@
+import 'package:clima/services/location.dart';
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location local = Location();
+    await local.getCurrentLocation();
+
+    print(local.longitude);
+    print(local.latitude);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
